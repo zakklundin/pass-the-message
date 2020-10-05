a0ba1c1622b111339829dda394ac120a9a19911f
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-10-05 15:54:29 +0200

    added functions to mutate input

--- script.js
+++ script.js
@@ -6,11 +6,50 @@ userInput.addEventListener("keyup", getInput)
 function getInput(event) {
 const text = event.target.value
 
-const result = manipulation(text)
-
+const result = statistik(text)
+//växla mellan = result och = text
 outputen.innerText = result
 }
+//nedan är olika funktioner som kan användas för att ändra det som blir output
+//string.split splittrar string till en array av strings
+//Array.join klistrar ihop en array av strings  till en enda lång string
 
-function manipulation(str) {
+function caesarChiffer(str) {
+    
+}
+
+function statistik(str) {
+    return "Antal tecken: " + str.length + " Antal ord: " + str.split(' ').length
+}
+
+function makeLouder (str) {
+ return str
+    .toUpperCase()
+    .split(' ')
+    .map(function(word) {
+        return word + '!'
+    })
+    .join(' ')
+}
+//samma som ovan fast klumpig (Ctrl + K + C för att göra kommentar, Ctrl + K + U tillbaka)
+// function louder(str) {
+//     const loudText = str.toUpperCase()
+//     const loudWords = loudText.split(' ')
+
+//     const foo = loudWords.map(function(word) {
+//         return word + '!'
+//     })
+//     return foo.join(' ')
+// }
+
+function doubleSwedish(str){
+    const words = str.split(' ')
+    console.log(words)
+
+    const duplicateWords = words.map(duplicate)
+    return duplicateWords.join(' ')
+}
+
+function duplicate (str){
     return str + str
 }
